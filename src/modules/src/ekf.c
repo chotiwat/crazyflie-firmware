d6f082cc998e859718bb45feb4046f015f6ce2ff
Chotiwat Chawannakul <chotiwat.c@hotmail.com>
2016-12-04 23:46:26 -0800

    decrease position variance, increase orientation variance

--- src/modules/src/ekf.c
+++ src/modules/src/ekf.c
@@ -25,10 +25,10 @@ void initUsecTimer() {}
 #endif
 
 // measured constants
-#define VICON_VAR_XY 1.5e-4 // 1.5e-5
+#define VICON_VAR_XY 1.5e-5 // 1.5e-5
 #define VICON_VAR_VEL 1 //2e-4
 // #define VICON_VAR_Z  1.0e-8
-#define VICON_VAR_Q  4.5e-1
+#define VICON_VAR_Q  9.0e-1
 #define GYRO_VAR_XYZ 0.2e-4
 // #define ACC_VAR_XY   1.5e-5
 // #define ACC_VAR_Z    3.9e-5
